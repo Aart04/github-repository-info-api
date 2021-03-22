08a25f9799098689acad501a80017d97552e9dc3
Jacek Dembowiak <jacekdembowiak@gmail.com>
2021-03-22 21:04:13 +0100

    add settings configuration for restAPI and app

--- github/settings.py
+++ github/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'repository.apps.RepositoryConfig'
 ]
 
 MIDDLEWARE = [
