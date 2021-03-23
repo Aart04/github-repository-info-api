86dfee273e127e55d5e392e0aeb0abb0d252e49f
Jacek Dembowiak <jacekdembowiak@gmail.com>
2021-03-23 21:43:06 +0100

    fix a bug with owner id and null language

--- repository/serializers.py
+++ repository/serializers.py
@@ -6,5 +6,5 @@ class RepositoryInfoSerializer(serializers.Serializer):
     full_name = serializers.CharField()
     description = serializers.CharField()
     clone_url = serializers.CharField()
-    owner_id = serializers.IntegerField()
-    language = serializers.CharField()
+    owner_id = serializers.DecimalField(max_digits=99, decimal_places=0)
+    language = serializers.CharField(allow_null=True)
